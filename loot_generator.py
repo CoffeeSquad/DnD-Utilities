ce2a6aef4d8f4984491499b9838449d12157f00a
Xtearra2004 <48783346+Xtearra2004@users.noreply.github.com>
2019-03-21 01:26:31 -0500

    created loot generator

---
+++ loot_generator.py
@@ -0,0 +1,17 @@
+import random
+
+# determining loot based off of Challenge Rating
+
+treasure_type = input("Is this an individual (I) or hoard (H) treasure: ")
+challenge_rating = input("Enter the Challenge Rating: ")
+
+if treasure_type == "I":
+    print("gay")
+elif treasure_type == "H":
+    print("hells yeah")
+
+
+
+
+
+
